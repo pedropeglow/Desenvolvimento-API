a324a2c788bf3da004636b5f9b9b0da35f6e047d
pedropeglow <pedropeglowm@gmail.com>
2023-10-16 13:08:08 -0300

    :memo: Adicionando porta.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const livroService = require('./service/livro_service')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
